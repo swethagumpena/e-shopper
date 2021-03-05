0009f1bece95d3a7ebb9c489c4dceb7340b94682
swetha <swetha.gumpena0607@gmail.com>
2021-03-05 23:24:35 +0530

    feat: Add Orders page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,12 +1,101 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-/**
-* @author
-* @function Orders
-* */
+const Orders = ({ orders }) => (
+  <div>
+    {/* <p>{JSON.stringify(orders)}</p> */}
+    <div className="basket-msg">
+      All Orders
+      <hr />
+      Past Orders (
+      {orders.length}
+      )
+    </div>
+    <div>
+      <table>
+        {orders.map((order) => (
+          <>
+            <tr key={order.id}>
+              <th>ORDER</th>
+              <th>ITEMS</th>
+              <th>DATE</th>
+              <th>AMOUNT</th>
+            </tr>
+            <tr>
+              <td>
+                Order id:
+                {' '}
+                {order.orderId}
+              </td>
+              <td>
+                {order.itemsCount}
+                {' '}
+                items
+              </td>
+              <td>{order.date}</td>
+              <td>{order.amount}</td>
+            </tr>
+            <tr>
+              <th>ITEM DESCRIPTION</th>
+              <th>UNIT PRICE</th>
+              <th>QUANTITY</th>
+              <th>SUB TOTAL</th>
+            </tr>
+            <tr className="category">
+              <td>FRUITS</td>
+              <td />
+              <td />
+              <td />
+            </tr>
+            {order.items.map((eachItem) => (
+              <>
+                <tr key={eachItem.id}>
+                  <td>{eachItem.productName}</td>
+                  <td className="items-align">
+                    Rs.
+                    {' '}
+                    {eachItem.price}
+                    .00
+                  </td>
+                  <td className="items-align">{eachItem.quantity}</td>
+                  <td className="items-align">
+                    Rs.
+                    {' '}
+                    {eachItem.price * eachItem.quantity}
+                    .00
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    TOTAL Rs.
+                    {' '}
+                    {order.items.reduce((accumulator, item) => (
+                      accumulator + eachItem.price * item.quantity), 0)}
+                    .00
+                  </td>
+                </tr>
 
-const Orders = () => (
-  <div>Orders</div>
+              </>
+            ))}
+
+          </>
+        ))}
+      </table>
+    </div>
+
+  </div>
 );
+
+const productShape = {
+  id: PropTypes.number,
+  productName: PropTypes.string,
+  quantity: PropTypes.number,
+  price: PropTypes.price,
+  url: PropTypes.string,
+};
+
+Orders.propTypes = {
+  orders: PropTypes.arrayOf(PropTypes.shape(productShape)).isRequired,
+};
 
 export default Orders;
