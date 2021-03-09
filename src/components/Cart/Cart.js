888227dbe81db6b2c4b7e1a77d06727c26f3e4a5
swetha <swetha.gumpena0607@gmail.com>
2021-03-09 08:41:10 +0530

    feat(useHistory): Routing to home page and checkout page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,86 +1,100 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Cart.scss';
-import { Link } from 'react-router-dom';
+// import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-const Cart = ({ cartItems }) => (
-  <div>
-    <div className="basket-msg">
-      Your Basket (
-      {cartItems.length}
-      {'  '}
-      items)
-      <hr />
-    </div>
-    <table className="basket-table">
-      <tbody>
-        <tr>
-          <th>ITEM DESCRIPTION</th>
-          <th>UNIT PRICE</th>
-          <th>QUANTITY</th>
-          <th>SUB TOTAL</th>
-        </tr>
-        <tr className="category">
-          <td>FRUITS</td>
-          <td />
-          <td />
-          <td />
-        </tr>
+const Cart = ({ cartItems }) => {
+  const history = useHistory();
+  const handleClickHome = () => {
+    history.push('/');
+  };
+  const handleClickCheckout = () => {
+    history.push('/checkout');
+  };
 
-        {cartItems.map((eachItem) => (
-          <tr key={eachItem.id}>
-            <td>{eachItem.productName}</td>
-            <td className="items-align">
-              Rs.
-              {' '}
-              {eachItem.price}
-              .00
-            </td>
-            <td className="items-align">{eachItem.quantity}</td>
-            <td className="items-align">
-              Rs.
-              {' '}
-              {eachItem.price * eachItem.quantity}
-              .00
-            </td>
+  return (
+
+    <div>
+      <div className="basket-msg">
+        Your Basket (
+        {cartItems.length}
+        {'  '}
+        items)
+        <hr />
+      </div>
+      <table className="basket-table">
+        <tbody>
+          <tr>
+            <th>ITEM DESCRIPTION</th>
+            <th>UNIT PRICE</th>
+            <th>QUANTITY</th>
+            <th>SUB TOTAL</th>
           </tr>
-        ))}
-      </tbody>
+          <tr className="category">
+            <td>FRUITS</td>
+            <td />
+            <td />
+            <td />
+          </tr>
 
-    </table>
+          {cartItems.map((eachItem) => (
+            <tr key={eachItem.id}>
+              <td>{eachItem.productName}</td>
+              <td className="items-align">
+                Rs.
+                {' '}
+                {eachItem.price}
+                .00
+              </td>
+              <td className="items-align">{eachItem.quantity}</td>
+              <td className="items-align">
+                Rs.
+                {' '}
+                {eachItem.price * eachItem.quantity}
+                .00
+              </td>
+            </tr>
+          ))}
+        </tbody>
 
-    <div className="total-container">
-      <div className="total-card">
-        <div className="total-amount">
-          <p>TOTAL</p>
-          <p>
-            Rs.
-            {' '}
-            {cartItems.reduce((accumulator, eachItem) => (
-              accumulator + eachItem.price * eachItem.quantity), 0)}
-            .00
-          </p>
+      </table>
+
+      <div className="total-container">
+        <div className="total-card">
+          <div className="total-amount">
+            <p>TOTAL</p>
+            <p>
+              Rs.
+              {' '}
+              {cartItems.reduce((accumulator, eachItem) => (
+                accumulator + eachItem.price * eachItem.quantity), 0)}
+              .00
+            </p>
+          </div>
+          <div className="horizontal-line">
+            <hr />
+          </div>
+          <div className="checkout-btn">
+            {/* <Link to="/checkout">
+              <button type="button">
+                CHECKOUT =&gt;
+              </button>
+            </Link> */}
+            <button type="button" onClick={handleClickCheckout}>CHECKOUT =&gt;</button>
+          </div>
         </div>
-        <div className="horizontal-line">
-          <hr />
-        </div>
-        <div className="checkout-btn">
-          <Link to="/checkout">
-            <button type="button">
-              CHECKOUT =&gt;
-            </button>
-          </Link>
+        <div className="continue-shopping">
+          {/* <Link to="/"><button type="button">Continue Shopping</button></Link> */}
+          <button type="button" onClick={handleClickHome}>Continue Shopping</button>
         </div>
       </div>
-      <div className="continue-shopping">
-        <Link to="/"><button type="button">Continue  Shopping</button></Link>
-      </div>
+
+      {/* <div>{JSON.stringify(cartItems)}</div> */}
     </div>
 
-    {/* <div>{JSON.stringify(cartItems)}</div> */}
-  </div>
-
-);
+  );
+};
 
 Cart.propTypes = {
   cartItems: PropTypes.arrayOf(PropTypes.shape({
